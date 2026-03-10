22e1fa5a712767789a2069cbdb401da2321b8918
ookuraseiya <seiya.ookura@gmail.com>
2026-03-11 00:06:29 +0900

    home page update

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,6 +1,6 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
-R.c("server/chunks/ssr/node_modules_e3c9fcc1._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__305743d6._.js")
+R.c("server/chunks/ssr/node_modules_next_dist_e5550e01._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__c98dcf8c._.js")
 R.c("server/chunks/ssr/node_modules_e2d1c5df._.js")
 R.c("server/chunks/ssr/[externals]__e8a2741f._.js")
 R.c("server/chunks/ssr/src_app_5b2047f8._.js")
